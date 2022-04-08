4749ef316a0155f55ab004b2e996f80b893146ca
Hari Nur Permadi <hari.permadi@prudential.co.id>
2022-04-08 11:59:34 +0700

    Initial Commit

---
+++ friendFaceApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  friendFaceApp
+//
+//  Created by Hari Permadi on 08/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ friendFaceApp/friendFaceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  friendFaceAppApp.swift
+//  friendFaceApp
+//
+//  Created by Hari Permadi on 08/04/22.
+//
+
+import SwiftUI
+
+@main
+struct friendFaceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
